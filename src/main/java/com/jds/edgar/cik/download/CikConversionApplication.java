692b99d2d86680d1e86ea77ad3557d6cd33474f1
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2023-04-04 07:48:36 +0400

    initial spring doc configuration

--- src/main/java/com/jds/edgar/cik/download/CikConversionApplication.java
+++ src/main/java/com/jds/edgar/cik/download/CikConversionApplication.java
@@ -1,10 +1,14 @@
 package com.jds.edgar.cik.download;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableScheduling
+@EnableWebMvc
+@OpenAPIDefinition
 @SpringBootApplication
 public class CikConversionApplication {
 
