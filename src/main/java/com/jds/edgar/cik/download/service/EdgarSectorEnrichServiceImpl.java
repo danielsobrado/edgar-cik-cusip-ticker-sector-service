b4787211881a251cc0449bceaadc97ba14f5bb79
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2023-04-08 03:56:46 +0400

    Handle  "No matching CIK." case

--- src/main/java/com/jds/edgar/cik/download/service/EdgarSectorEnrichServiceImpl.java
+++ src/main/java/com/jds/edgar/cik/download/service/EdgarSectorEnrichServiceImpl.java
@@ -57,9 +57,19 @@ public class EdgarSectorEnrichServiceImpl {
 
         // Check if "No matching Ticker Symbol." is present in the HTML content
         if (doc.text().contains("No matching Ticker Symbol.")) {
+            log.warn("No matching Ticker Symbol for CIK: {}", ticker);
             return Stock.EnrichedData.builder()
                     .sic("Not Found")
                     .sector("Not Found")
+                    .build();
+        }
+
+        // Check if "No matching CIK." is present in the HTML content
+        if (doc.text().contains("No matching CIK.")) {
+            log.warn("No matching CIK for CIK: {}", ticker);
+            return Stock.EnrichedData.builder()
+                    .sic("No CIK")
+                    .sector("No CIK")
                     .build();
         }
 
